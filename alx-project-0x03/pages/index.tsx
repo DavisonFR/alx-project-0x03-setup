c5532303fbc7acddaacbcebbbe1324157f2e2a6d
Dave <dawitfirew439@gmail.com>
2025-06-08 15:43:45 +0300

    Updated homepage content

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
-   <div className="flex items-center justify-center h-screen bg-gray-100 text-center">
-    <h1 className="text-4xl font-bold text-gray-800">Welcome to Splash App</h1>
+   <div>
+    <h1> Welcome to Splash App </h1>
    </div>
   );
 };
